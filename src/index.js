37c0f135ae50378e9e47eb7fed427520b97f4bab
Oleksandr Dudynets <hello@dudynets.dev>
2023-09-24 23:53:38 +0300

    feature: add username validation, improve IO

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import chalk from 'chalk';
 
 // Function to prompt for user or organization and GitHub username
 async function getUserInput() {
-  const userInput = await inquirer.prompt([
+  const input = await inquirer.prompt([
     {
       type: 'list',
       name: 'entity',
@@ -20,25 +20,76 @@ async function getUserInput() {
     },
   ]);
 
+  input.username = input.username.trim();
+  input.username = input.username.replace(/^@/g, '');
+
+  if (
+    !input.username ||
+    !input.username.match(
+      /\B([a-z0-9](?:-(?=[a-z0-9])|[a-z0-9]){0,38}(?<=[a-z0-9]))/gi,
+    )
+  ) {
+    console.error(chalk.red('\nℹ Please enter a valid GitHub username.'));
+    process.exit(1);
+  }
+
+  return input;
+}
+
+// Function to fetch repositories
+async function fetchRepositories(entity, username) {
   console.log(
     chalk.yellow(
-      `\nCloning ${userInput.entity.toLowerCase()} repositories for ${
-        userInput.username
-      }...`,
+      `\nℹ Fetching ${entity.toLowerCase()} repositories for @${username}...`,
     ),
   );
 
-  return userInput;
-}
+  const entityMap = new Map([
+    ['User', 'users'],
+    ['Organization', 'orgs'],
+  ]);
 
-// Function to fetch and clone repositories
-async function cloneRepositories(entity, username) {
   try {
     const response = await axios.get(
-      `https://api.github.com/${entity.toLowerCase()}s/${username}/repos`,
+      `https://api.github.com/${entityMap.get(entity)}/${username}/repos`,
+    );
+    const repositories = response.data;
+
+    if (repositories.length === 0) {
+      console.error(
+        chalk.red(`\nℹ ${entity} @${username} has no public repositories.`),
+      );
+      process.exit(1);
+    }
+
+    return repositories;
+  } catch (error) {
+    if (error.response.status === 404) {
+      console.error(
+        chalk.red(
+          `\nℹ ${entity} @${username} not found. Please check the username and try again.`,
+        ),
+      );
+    } else {
+      console.error(
+        chalk.red(
+          `\nℹ ${entity} @${username} could not be fetched. Please check the username and try again.`,
+        ),
+      );
+    }
+    process.exit(1);
+  }
+}
+
+// Function to clone repositories
+async function cloneRepositories(repositories, entity, username) {
+  try {
+    console.log(
+      chalk.yellow(
+        `ℹ Cloning ${entity.toLowerCase()} repositories for @${username}...`,
+      ),
     );
 
-    const repositories = response.data;
     const outputFolder = `dumps/${entity.toLowerCase()}_${username}`;
 
     // Delete the folder if it already exists
@@ -46,6 +97,12 @@ async function cloneRepositories(entity, username) {
 
     // Create the folder
     await fs.mkdirp(outputFolder);
+
+    // Create a README.md file with the entity name and username and the repository list
+    const readme = `# ${entity} @${username}\n\n## Repositories\n\n${repositories
+      .map((repo) => `- [${repo.name}](${repo.html_url})`)
+      .join('\n')}`;
+    await fs.writeFile(`${outputFolder}/README.md`, readme);
 
     // Clone repositories with progress
     for (const repo of repositories) {
@@ -69,9 +126,12 @@ async function cloneRepositories(entity, username) {
       });
     }
 
-    console.log('\n', chalk.green('✔ All repositories cloned successfully.'));
+    console.log();
+    console.log(chalk.green('✔ All repositories cloned successfully.'));
   } catch (error) {
-    console.error('\n', chalk.red('ℹ An error occurred:'), error);
+    console.log();
+    console.error(chalk.red('ℹ An error occurred:'), error.message);
+    process.exit(1);
   }
 }
 
@@ -82,7 +142,8 @@ async function main() {
   const userInput = await getUserInput();
   const {entity, username} = userInput;
 
-  await cloneRepositories(entity, username);
+  const repositories = await fetchRepositories(entity, username);
+  await cloneRepositories(repositories, entity, username);
 }
 
 main();
